ab5e529552fdaae7840c01bb60b365bc63151dd1
Andrew Maldonado <andrew.maldonado@mrjaffesclass.com>
2023-05-11 12:46:24 -0700

    Make spawnRandom and setNum

--- src/mvc/twentyfortyeight/Model.java
+++ src/mvc/twentyfortyeight/Model.java
@@ -16,6 +16,7 @@ public class Model implements MessageHandler {
   private final Messenger mvcMessaging;
   private int gameOver;
   String[][] board;
+  int[][] intBoard;
   
 
   // Model's data vControllerariables
@@ -28,6 +29,7 @@ public class Model implements MessageHandler {
   public Model(Messenger messages) {
     mvcMessaging = messages;
     this.board = new String[4][4];
+    this.intBoard = new int[4][4];
   }
   
   /**
@@ -41,10 +43,12 @@ public class Model implements MessageHandler {
   
     //reset board and for new game
     public void newGame() {
+      spawnRandom();
+      spawnRandom();
         for(int row=0; row<this.board.length; row++) {
             for (int col=0; col<this.board[0].length; col++) {
-            this.board[row][col] = "";
-            }
+              this.board[row][col] = "";
+            }board
         }
     }
     
@@ -82,6 +86,31 @@ public class Model implements MessageHandler {
     }
   }
   
+  public void spawnRandom() {
+    boolean torf = false;
+    //make chance for 1 to be 4  
+    while(!torf) {
+        int ranRow = (int)(Math.random() * 3);
+        int ranCol = (int)(Math.random() * 3);
+        if(intBoard[ranRow][ranCol] == 0) {
+          intBoard[ranRow][ranCol] = 2;
+          torf = true;
+        }
+      }
+  }
+
+  public void setNum() {
+    for(int i = 0; i < 3; i++) {
+      for(int j = 0; j < 3; j++) {
+        String value = String.valueOf(intBoard[i][j]);
+        if(value == "0") {
+          value = "";
+        }
+        board[i][j] = value;
+      }
+    }
+  }
+
   
   
   
